d3f8ccb384f80ae5a7ae64aae5e9b712e6fef603
AShakhmatov <Coluchy@yandex.ru>
2024-01-30 03:43:16 +0600

    new code of tests

---
+++ src/test/java/tests/PageObjectsPracticeFormTests.java
@@ -0,0 +1,52 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+public class PageObjectsPracticeFormTests extends TestBase {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void fullRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName("Aleksandr")
+                .setLastName("Exile")
+                .setEmail("AleksandrExile@gmail.com")
+                .setGender("Male")
+                .setUserNumber("9001122999")
+                .setDateOfBirth("30", "July", "2008")
+                .setSubjectsInput("Arts")
+                .setHobbiesWrapperInput("Sports")
+                .setUploadPictureInput("9.png")
+                .setCurrentAddress("www.Leningrad.spb.ru")
+                .setStateWrapper("Rajasthan")
+                .setCityWrapper("Jaipur")
+                .submit()
+                .checkResult("Student Name", "Aleksandr Exile")
+                .checkResult("Student Email", "AleksandrExile@gmail.com")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "900112299")
+                .checkResult("Date of Birth", "30 July,2008")
+                .checkResult("Subjects", "Arts")
+                .checkResult("Hobbies", "Sports")
+                .checkResult("Picture", "9.png")
+                .checkResult("Address", "www.Leningrad.spb.ru")
+                .checkResult("State and City", "Rajasthan Jaipur");
+    }
+
+
+    @Test
+    void minimalRegistrationTest() {
+        registrationPage.openPage()
+                .setFirstName("Aleksandr")
+                .setLastName("Exile")
+                .setGender("Male")
+                .setUserNumber("9001122999")
+                .submit()
+                .checkResult("Student Name", "Aleksandr Exile")
+                .checkResult("Gender", "Male")
+                .checkResult("Mobile", "9001122999");
+
+    }
+}
---
+++ src/test/java/tests/PracticeFormTests.java
@@ -0,0 +1,45 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void successfulRegistrationTest() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Aleksandr");
+        $("#lastName").setValue("Exile");
+        $("#userEmail").setValue("AleksandrExile@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("900112299");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("6");
+        $(".react-datepicker__year-select").selectOption("1988");
+        $(".react-datepicker__day--009:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("img/9.png");
+        $("#currentAddress").setValue("www.Leningrad.spb.ru");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Rajasthan")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Jaipur")).click();
+        $("#submit").click();
+    }
+}
+
